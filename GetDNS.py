ab63aa6ffacf542e168f5a65534a0976ffcf81a6
孙力辉(Sunlihui) <365793275@qq.com>
2017-10-31 11:25:47 +0800

    Update GetDNS.py

--- GetDNS.py
+++ GetDNS.py
@@ -17,9 +17,9 @@ else:
 
 # Create a connection to the server:
 connection = sdk.Connection(
-	url='https://ovirt-master/ovirt-engine/api',
-	username='monitor@internal',
-	password='PLOKMj#H864',
+	url='https://',
+	username='',
+	password='',
 	insecure=True,
 	)
 if connection =="":
@@ -30,35 +30,8 @@ system_service = connection.system_service()
 vms_service = system_service.vms_service()
 vms = vms_service.list()
 
-SkipList = ["38-210-cdh-1",
-            "36-210-cdh-2",
-            "47-210-cdh-3",
-            "33-210-cdh-4",
-            "31.001-mysql001",
-            "36.001-mysql02",
-            "33.001-mysql003",
-            "38.001-mysql04",
-            "34.100-vpn",
-            "36.99-hi.tuhu.com",
-            "38.100-SA",
-            "38.200-gaoyu",
-            ]
-KvmList = ["10.1.1.38 kvm-1.ad.tuhu.cn",
-"10.1.1.36 kvm-2.ad.tuhu.cn",
-"10.1.1.47 kvm-3.ad.tuhu.cn",
-"10.1.1.33 kvm-4.ad.tuhu.cn",
-"10.1.1.31 kvm-5.ad.tuhu.cn",
-"10.1.1.34 kvm-6.ad.tuhu.cn",
-"10.1.1.43 kvm-7.ad.tuhu.cn",
-"10.1.1.74 kvm-8.ad.tuhu.cn",
-"10.1.1.73 kvm-9.ad.tuhu.cn",
-"10.1.1.72 kvm-10.ad.tuhu.cn",
-"10.1.1.71 kvm-11.ad.tuhu.cn",
-"10.1.1.65 kvm-12.ad.tuhu.cn",
-"10.1.1.66 kvm-13.ad.tuhu.cn",
-"10.1.1.67 kvm-14.ad.tuhu.cn",
-"10.1.38.100 yum",
-	  ]
+SkipList = []
+KvmList = []
 
 dnshosts = {}
 hosts = []
